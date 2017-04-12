2fee13b3e6521baa7e2c05cae8d3014bdad50ea2
imranz30 <imran.zafar81@gmail.com>
2017-04-12 14:33:29 +0200

    Image search

---
+++ vacation-image-search-engine/vacation-image-search-engine/search.py
@@ -0,0 +1,44 @@
+# USAGE
+# python search.py --index index.csv --query queries/103100.png --result dataset
+
+# import the necessary packages
+from pyimagesearch.colordescriptor import ColorDescriptor
+from pyimagesearch.searcher import Searcher
+import argparse
+import cv2
+
+# construct the argument parser and parse the arguments
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--index", required = True,
+	help = "Path to where the computed index will be stored")
+ap.add_argument("-q", "--query", required = True,
+	help = "Path to the query image")
+ap.add_argument("-r", "--result", required = True,
+	help = "Path to the result path")
+args = vars(ap.parse_args())
+
+# initialize the image descriptor
+cd = ColorDescriptor((8, 12, 3))
+
+# load the query image and describe it
+query = cv2.imread(args["query"])
+
+# display the query
+cv2.imshow("Query", query)
+
+features = cd.describe(query)
+
+# perform the search
+searcher = Searcher(args["index"])
+results = searcher.search(features)
+
+# loop over the results
+
+for (score, resultID) in results:
+	# load the result image and display it
+    print resultID
+    result = cv2.imread("dataset/"+resultID)
+    cv2.imshow("Result", result)
+    cv2.waitKey(0)
+cv2.destroyAllWindows()
